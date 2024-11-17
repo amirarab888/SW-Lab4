2f740077753c62f27e0ea8451e7511e84233fe4e
amkamir82 <amk_amir82@yahoo.com>
2024-11-18 01:46:40 +0330

    add dependencies to pox.xml

---
+++ SW-Lab4/src/test/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("Two input values, {int} and {int}")
+    public void twoInputValuesAnd(int arg0, int arg1) {
+    }
+
+    @When("I add the two values")
+    public void iAddTheTwoValues() {
+    }
+
+    @Then("I expect the result {int}")
+    public void iExpectTheResult(int arg0) {
+    }
+}

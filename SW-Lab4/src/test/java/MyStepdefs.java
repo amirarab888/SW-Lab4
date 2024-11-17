2562fb7f9028a1fbd187b72c225c3a04e6ae7391
amkamir82 <amk_amir82@yahoo.com>
2024-11-18 01:48:01 +0330

    passed first tests for first scenario

--- SW-Lab4/src/test/java/MyStepdefs.java
+++ SW-Lab4/src/test/java/MyStepdefs.java
@@ -1,17 +1,36 @@
+import org.example.Calculator;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.Before;
+
+import org.junit.Assert;
 
 public class MyStepdefs {
+    private Calculator calculator;
+    private int value1;
+    private int value2;
+    private int result;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
     @Given("Two input values, {int} and {int}")
     public void twoInputValuesAnd(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
     }
 
     @When("I add the two values")
     public void iAddTheTwoValues() {
+        result = calculator.add(value1, value2);
     }
 
     @Then("I expect the result {int}")
     public void iExpectTheResult(int arg0) {
+        Assert.assertEquals(arg0, result);
     }
 }
